c5d66cced8fc906743333ced80db24105499091e
Michael Starks <starksmichael224@gmail.com>
2024-09-19 20:00:32 -0600

    Set leader key to <space> and set line numbers by default

--- init.lua
+++ init.lua
@@ -1,2 +1,10 @@
+--Top level key maps that need to be loaded before lazy
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>op", "<CMD>Oil<CR>",{ desc = "Open parent directory" })
+
 require("michael.core")
 require("michael.lazy")
+
+-- Set numbers to be one by default
+vim.wo.number = true
+
